ebf65a6e779bbb0320936b5cc5a378489b19d097
blupie01 <jeffrey.chou.89@gmail.com>
2017-01-11 16:39:16 -0500

    partial single patient page

---
+++ Display_single_patient.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCxjZqhAWrp3vf_wanuS7_iYem_1eLattA",
+    authDomain: "inpatient-8a288.firebaseapp.com",
+    databaseURL: "https://inpatient-8a288.firebaseio.com",
+    storageBucket: "inpatient-8a288.appspot.com",
+    messagingSenderId: "607664151265"
+};
+firebase.initializeApp(config);
+
+inPatientDatabase = firebase.database();
